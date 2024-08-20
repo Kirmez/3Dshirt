d23a64b1f93f6312299ae06644e561ac71ad18ba
Kirill Mezentsev <kirmez02@gmail.com>
2024-08-20 23:02:05 +0200

    fixed text

--- client/src/components/CustomButton.jsx
+++ client/src/components/CustomButton.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import state from "../store";
 import { useSnapshot } from "valtio";
 
+import { getContrastingColor } from "../config/helpers";
+
 const CustomButton = ({ type, title, customStyles, handleClick }) => {
   const snap = useSnapshot(state);
 
@@ -9,7 +11,13 @@ const CustomButton = ({ type, title, customStyles, handleClick }) => {
     if (type === "filled") {
       return {
         backgroundColor: snap.color,
-        color: "#fff",
+        color: getContrastingColor(snap.color),
+      };
+    } else if (type === "outline") {
+      return {
+        borderWidth: "1px",
+        borderColor: snap.color,
+        color: snap.color,
       };
     }
   };
@@ -17,7 +25,8 @@ const CustomButton = ({ type, title, customStyles, handleClick }) => {
   return (
     <button
       className={`px-2 py-1.5 flex-1 rounded-md ${customStyles}`}
-      style={generateStyle(type)} onClick={handleClick}>
+      style={generateStyle(type)}
+      onClick={handleClick}>
       {title}
     </button>
   );
